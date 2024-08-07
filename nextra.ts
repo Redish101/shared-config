7d360c7fd00e1d11b4a4ca26d1696dcd08dd8dee
Redish101 <i@redish101.top>
2024-08-07 10:43:21 +0800

    init

---
+++ nextra.ts
@@ -0,0 +1,23 @@
+export default {
+  search: {
+    placeholder: "搜索",
+  },
+  toc: {
+    title: "目录",
+  },
+  feedback: {
+    content: "发现问题？提交反馈 →",
+  },
+  editLink: {
+    text: "编辑此页",
+  },
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: "亮色",
+        dark: "暗色",
+        system: "跟随系统",
+      };
+    },
+  },
+};
